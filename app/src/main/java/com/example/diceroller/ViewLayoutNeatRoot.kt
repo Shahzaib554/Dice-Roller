e8b9782598c0fcc6cf5601278cecd7f7b985eb59
Shahzaib554 <86713644+Shahzaib554@users.noreply.github.com>
2025-08-22 18:01:08 +0500

    Image added and some layout work

---
+++ app/src/main/java/com/example/diceroller/ViewLayoutNeatRoot.kt
@@ -0,0 +1,16 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ViewLayoutNeatRoot : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_view_layout_neat_root)
+
+    }
+}
